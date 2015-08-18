1d9126921463d9303c5227958389df275c45d4f0
Doug Jackson <dougjack@dougjackson.net>
2015-08-17 22:09:57 -0700

    Initial modification to calculate the inverse instead of the mean. Removed the nasty implicit returns and dangerous missing curly braces.

--- cachematrix.R
+++ cachematrix.R
@@ -7,28 +7,39 @@
 # Make a list which is sort of a bastardized object with "methods"
 # to get and set the value of a matrix as well as its inverse
 makeCacheMatrix <- function(x = matrix()) {
-    m <- NULL
-    set <- function(y) {
-        x <<- y
-        m <<- NULL
+    invMat <- NULL
+    
+    # Set the data and clear invMat
+    set <- function(m) {
+        mat <<- m
+        invMat <<- NULL
     }
-    get <- function() x
-    setmean <- function(mean) m <<- mean
-    getmean <- function() m
-    list(set = set, get = get, setmean = setmean, getmean = getmean)
+    
+    get <- function() {return(mat)}
+    
+    setInv <- function(iM) {invMat <<- iM}
+    
+    getInv <- function() {return(invMat)}
+    
+    return(list(set = set, get = get, setInv = setInv, getInv = getInv))
 }
 
 # Obtain the inverse of a matrix, either by pulling it from the cache
 # or calculating it if it's not already in the cache
 cacheSolve <- function(x, ...) {
+    
     # Return a matrix that is the inverse of x
-    m <- x$getmean()
+    m <- x$getInv()
+    
+    # Pull the inverse from cache if it's there
     if(!is.null(m)) {
-            message("getting cached data")
-            return(m)
+        cat("Getting cached data.\n")
+        return(m)
     }
+    
+    # Otherwise, calculate the inverse
     data <- x$get()
-    m <- mean(data, ...)
-    x$setmean(m)
-    m
+    m <- solve(data, ...)
+    x$setInv(m)
+    return(m)
 }
